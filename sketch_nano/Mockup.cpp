66f427642f394857e41cae489514e1f470982a8f
Chaudois <damien.chaudois@gmail.com>
2019-05-01 10:21:02 +0200

    First version of the testing environment for the game interface

---
+++ sketch_nano/Mockup.cpp
@@ -0,0 +1,5 @@
+//
+// Created by d.chaudois on 01/05/2019.
+//
+
+#include "Mockup.h"
---
+++ sketch_nano/Mockup.h
@@ -0,0 +1,16 @@
+//
+// Created by d.chaudois on 01/05/2019.
+//
+
+#ifndef SKETCH_NANO_MOCKUP_H
+#define SKETCH_NANO_MOCKUP_H
+
+
+
+class Mockup {
+
+};
+
+
+
+#endif //SKETCH_NANO_MOCKUP_H
