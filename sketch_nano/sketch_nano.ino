5022cd77e95d6b6f7f254b8fc5227fec2acba2d8
TristanEduProjet <TristanEduProjet@users.noreply.github.com>
2019-05-03 18:14:04 +0200

    Add debug

--- sketch_nano/sketch_nano.ino
+++ sketch_nano/sketch_nano.ino
@@ -27,6 +27,7 @@ void setup() {
 }
 
 void loop() {
+    check_serial();
     static Coord btn, prev_btn = {UINT8_MAX, UINT8_MAX};
     //Check if a button is pressed
     btn = g->c->check_button();
@@ -43,4 +44,77 @@ void loop() {
         prev_btn = btn;
     }
     delay(100);
+}
+
+void check_serial() {
+    if(Serial.available() > 0) {
+        if(Serial.read() == 'd') {
+            if(Serial.peek() == '\n') Serial.read();
+            Serial.println("Debug mode loop");
+            bool _continue = true;
+            do {
+                while(Serial.available() <= 0) delay(10);
+                switch(Serial.peek()) {
+                    case 'o':
+                        Serial.read();
+                        if(Serial.peek() == '\n') Serial.read();
+                        while(Serial.available() <= 0) delay(10);
+                        if(Serial.peek() == 'n') {
+                            fill_solid(c->leds, 24, CRGB::White);
+                            FastLED.show();
+                        } else if(Serial.peek() == 'f') {
+                            FastLED.clear();
+                            FastLED.show();
+                        } else {
+                          Serial.println("Not recognize");
+                        }
+                        break;
+                    case 't':
+                        Serial.println("Test mode");
+                        c->blink = false;
+                        Coord prev = {UINT8_MAX, UINT8_MAX};
+                        uint8_t i;
+                        do {
+                          #define sub(AX, txt) digitalWrite(AX, HIGH); \
+                            for(i=2 ; i <= 7 ; i++) { \
+                              if(digitalRead(i) == HIGH) { \
+                                Serial.print("input "); \
+                                Serial.print(txt); \
+                                Serial.print(' '); \
+                                Serial.println(i); \
+                              } \
+                            } \
+                            digitalWrite(AX, LOW);
+                            sub(A1, "A1");
+                            sub(A2, "A2");
+                            sub(A3, "A3");
+                            sub(A4, "A4");
+                            /*const Coord pos = c->check_button();
+                            if(pos.x == prev.x && pos.y == prev.y) {
+                                c->disactivateLED(pos.x, pos.y);
+                                prev = pos;
+                                if(pos.x != UINT8_MAX) {
+                                  Serial.print("btn ");
+                                  Serial.print(pos.x);
+                                  Serial.print(' ');
+                                  Serial.println(pos.y);
+                                  c->activateLED(pos.x, pos.y, 1);
+                                }
+                            }*/
+                        } while(Serial.read() != 's');
+                        break;
+                    case 'q':
+                        _continue = false;
+                        break;
+                    default:
+                        Serial.println("Command not recognize");
+                }
+                Serial.read();
+                if(Serial.peek() == '\n') Serial.read();
+            } while(_continue);
+            Serial.println("Exiting Debug mode");
+        } else {
+            Serial.println("Bad input");
+        }
+    }
 }
